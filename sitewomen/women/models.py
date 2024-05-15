299ce14fd5af44621579d18455cb43ad64630ce8
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-05-15 17:00:09 +0300

    Миграция модели women

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -9,5 +9,4 @@ class Women(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.title
+   
