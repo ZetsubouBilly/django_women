a1806065e1a53e51245fe0b2065b619b5c9b428e
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-05-16 13:26:46 +0300

    Создание пользовательского менеджера модели

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -3,13 +3,22 @@ from django.urls import reverse
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
+
 class Women(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
 
     def __str__(self):
         return self.title
@@ -19,6 +28,9 @@ class Women(models.Model):
         indexes = [
             models.Index(fields=['-time_create'])
         ]
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -31,7 +31,7 @@ cats_db = [
 
 # Create your views here.
 def index(request):
-    posts = Women.objects.filter(is_published=1)
+    posts = Women.published.all()
     data = {
         "title": "Главная страница",
         "menu": menu,
