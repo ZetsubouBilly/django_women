cb8af64272f5a31e678f8569b77d9aef5e3d7480
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-05-20 13:08:17 +0300

    Добавление тегов на сайт, breadcrambs к тегам, new template, маршрут к тегам

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from women.models import Category
+from women.models import Category, TagPost
 import women.views as views
 
 register = template.Library()
@@ -11,3 +11,10 @@ register = template.Library()
 def show_categories(cat_selected=0):
     cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected}
+
+
+
+@register.inclusion_tag('women/list_tags.html')
+def show_all_tags():
+    
+    return {'tags': TagPost.objects.all()}
