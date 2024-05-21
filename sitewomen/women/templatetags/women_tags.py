2e2f2a48982da2e8b0d4ba19a1f0a110ec0db287
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-05-21 13:39:23 +0300

    отображение тегов и категорий по наличию постов

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from women.models import Category, TagPost
 import women.views as views
 
@@ -9,7 +10,7 @@ register = template.Library()
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
@@ -17,4 +18,4 @@ def show_categories(cat_selected=0):
 @register.inclusion_tag('women/list_tags.html')
 def show_all_tags():
     
-    return {'tags': TagPost.objects.all()}
+    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
