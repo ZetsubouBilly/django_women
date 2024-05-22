c71e16c0b3686ac8b95b57a7d717497997bca56f
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-05-22 16:03:48 +0300

    Настройка формы редактирования записей в админ-панели, поля при добавлении и отображении, костыль с чпу

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 #  Women.objects.filter(Q(pk__in=[1,2,5])| Q(cat_id=2), title__icontains="ра")
 # Women.objects.filter(pk__gt=F("cat_id"))
 # lst = Husband.objects.all().annotate(is_married=Value(True))
+
+def translit_to_eng(s: str) -> str:
+    d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+         'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'к': 'k',
+         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+         'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya'}
+
+    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
@@ -42,6 +53,10 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(translit_to_eng( self.title))
+    #     super().save(*args, **kwargs)
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Название категории")
