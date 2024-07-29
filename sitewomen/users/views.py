fa333a5286fa8c0b910f41fe581903208897465a
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-07-29 14:01:19 +0300

    переписал регистрацию на класс UserCreationForm

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from .forms import LoginUserForm, RegisterUserForm
 
@@ -22,15 +23,21 @@ def logout_user(request):
     return HttpResponseRedirect(reverse("users:login"))
 
 
-def register(request):
-    if request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data["password"])
-            user.save()
-            return render(request, "users/register_done.html")
-    else:
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': 'Регистрация'}
+    success_url = reverse_lazy('users:login')
 
-        form = RegisterUserForm()
-    return render(request, "users/register.html", {"form": form})
+# def register(request):
+#     if request.method == "POST":
+#         form = RegisterUserForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.set_password(form.cleaned_data["password"])
+#             user.save()
+#             return render(request, "users/register_done.html")
+#     else:
+
+#         form = RegisterUserForm()
+#     return render(request, "users/register.html", {"form": form})
