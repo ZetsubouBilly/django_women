764d35e9c9b3bf8df7aecbc83c651613a2791b52
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-08-14 12:19:10 +0300

    добавил пакеты для эмуляции https соединения

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*sp6l(-_k!n)uk4-t*s=m@!x9z*o7egqo9g3fx&v3588%y#^q&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sitewomen.ru']
 INTERNAL_IPS = ["127.0.0.1",]
 
 
