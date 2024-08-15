d17914de303de40ebed4bd9859f329b3825dc233
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-08-15 12:12:37 +0300

    подключил postgresql, преенес все данные из json, установил пакет для работы с postgresql

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -86,9 +86,19 @@ WSGI_APPLICATION = 'sitewomen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sitewomen_db',
+        'USER': 'sitewomen',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    
 }
 
 
