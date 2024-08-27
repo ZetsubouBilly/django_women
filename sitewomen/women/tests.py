c785834c60103ffbb11fcb6bb05c96ceba14e1e8
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-08-27 15:59:05 +0300

    создал несколько фикстур бд для проверки тестов связаных с бд

--- sitewomen/women/tests.py
+++ sitewomen/women/tests.py
@@ -2,9 +2,13 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from .models import Women
+
 # Create your tests here.
 
 class GetPagesTestCase(TestCase):
+    fixtures= ['women_women.json', 'women_category.json', 'women_husband.json', 'women_tagpost.json']
+
     def setUp(self):
         "sad"
     
@@ -24,7 +28,26 @@ class GetPagesTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, redirect_uri)
 
-    
+    def test_data_mainpage(self):
+        w_lst = Women.published.all().select_related("cat")
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertQuerysetEqual(response.context_data['posts'], w_lst[:5])
+
+    def test_paginate_mainpage(self):
+        path = reverse('home')
+        page = 2
+        paginate_by =5;
+        response = self.client.get(path + f'?page={page}')
+        w_lst = Women.published.all().select_related("cat")
+        self.assertQuerysetEqual(response.context_data['posts'], w_lst[(page-1)*paginate_by:page*paginate_by])
+
+    def test_content_post(self):
+        w = Women.published.get(pk=1)
+        path = reverse('post', args=[w.slug])
+        response = self.client.get(path)
+        self.assertEqual(w.content, response.context_data['post'].content)
+
 
     def tearDown(self):
         "aad"
