2209d58ea3dbdda84f1187f782ecd9b09e83afcb
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-08-27 16:48:25 +0300

    добавил несколько тестов для приложения users

--- sitewomen/users/tests.py
+++ sitewomen/users/tests.py
@@ -1,3 +1,46 @@
+from http import HTTPStatus
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class RegisterUserTestCase(TestCase):
+    def setUp(self):
+        self.data = {
+            'username': 'testUser1',
+            'email': 'testuser@test.com',
+            'first_name': 'Tester',
+            'last_name': 'Testovski',
+            'password1': '12345678aA',
+            'password2': '12345678aA'
+        }
+    def test_form_registration_get(self):
+        path = reverse('users:register')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_user_registration_success(self):
+        
+        user_model = get_user_model()
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(user_model.objects.filter(username=self.data['username']).exists())
+
+    def test_user_registration_password_error(self):
+        self.data['password2'] = '12345678a'
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, "Введенные пароли не совпадают")
+
+    def test_user_registration_exists_error(self):
+        user_model = get_user_model()
+        user_model.objects.create(username=self.data['username'])
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, "Пользователь с таким именем уже существует")
+
