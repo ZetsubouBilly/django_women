2a6c895ce40ab586ea1bcc99081d16c1d6bf5eca
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-09-04 14:16:02 +0300

    переписал сериализатор на связанный с моделью women

--- sitewomen/women/serializers.py
+++ sitewomen/women/serializers.py
@@ -9,34 +9,10 @@ from .models import Women
 #         self.title = title
 #         self.content = content
 
-class WomenSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=255)
-    slug = serializers.SlugField(max_length=255)
-    photo = serializers.ImageField(required=False, allow_null=True, allow_empty_file=True)  # Позволяет `null` и `blank`
-    content = serializers.CharField(required=False)  # Позволяем пустой контент
-    time_create = serializers.DateTimeField(read_only=True)  # Чтение только
-    time_update = serializers.DateTimeField(read_only=True)  # Чтение только
-    is_published = serializers.BooleanField(default=False)  # Устанавливаем по умолчанию на DRAFT
-    cat_id = serializers.IntegerField()  # Идентификатор категории
-    # tags = serializers.ListField(child=serializers.CharField(), required=False)  # Список идентификаторов тегов
-    husband_id = serializers.IntegerField(required=False, allow_null=True)  # Идентификатор мужа
-    author_id = serializers.IntegerField(required=False, allow_null=True)  # Идентификатор автора
-
-    def create(self, validated_data):
-        return Women.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.slug = validated_data.get('slug', instance.slug)
-        instance.photo = validated_data.get('photo', instance.photo)
-        instance.content = validated_data.get('content', instance.content)
-        instance.time_update = validated_data.get('time_update', instance.time_update)
-        instance.is_published = validated_data.get('is_published', instance.is_published)
-        instance.cat_id = validated_data.get('cat_id', instance.cat_id)
-        instance.husband_id = validated_data.get('husband_id', instance.husband_id)
-        instance.author_id = validated_data.get('author_id', instance.author_id)
-        instance.save()
-        return instance
+class WomenSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Women
+      fields = '__all__'
 
 # def encode():
 #     model = WomenModel('Andelina', 'Content: Angelina bio')
